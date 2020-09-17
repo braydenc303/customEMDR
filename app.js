af01818087061f53be0d90b6a0848ed6e9021eb9
Brayden Copley <braydenc303@gmail.com>
2020-09-17 13:42:12 -0600

    Text readable with all background colors

--- app.js
+++ app.js
@@ -8,6 +8,26 @@ document.getElementById("bpm").addEventListener("change", chooseBpm);
 
 function chooseBackground() {
     background = document.getElementById("colorPicker1").value;
+    let brightness = 0
+    for (let i = 1; i < background.length; i++) {
+        console.log(background[i])
+        console.log(parseInt(background[i]))
+        console.log(typeof parseInt(background[i]))
+        if (parseInt(background[i]) !== 0 && !parseInt(background[i])) {
+            brightness++;
+        }
+    }
+    console.log(brightness);
+    if (brightness < 3) {
+        document.getElementById("box2").style.color = "#FFFFFF"
+        document.getElementById("box3").style.color = "#FFFFFF"
+        document.getElementById("box4").style.color = "#FFFFFF"
+    } else {
+        document.getElementById("box2").style.color = "#000000"
+        document.getElementById("box3").style.color = "#000000"
+        document.getElementById("box4").style.color = "#000000"
+    }
+    console.log(parseInt(background[5]));
     console.log(background);
     document.body.style.background = background;
 }
